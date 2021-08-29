857b050ff666c4bf54a70d458930931030101418
Shota-Amano <ab18004@shibaura-it.ac.jp>
2021-08-29 15:36:14 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!--scripts-->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        
+        <title>Laravel</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+         <div>
+            <h3 class="title">Idea掲示板</h3>
+        </div>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="desc col-md-8 offset-md-2">
+                    <p>この掲示板では、「こういうアプリがあったらいいな」などといった意見や、普段困っていることを募集しています。</p>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="posts container-fluid">
+            <div class="row">
+                <div class="post col-md-5 offset-md-1">
+                    <div class="post_frame">
+                        <p class="post_title">タイトル</p>
+                        <p class="post_body">本文</p>
+                    </div>
+                </div>
+                <div class="post col-md-5">
+                    <div class="post_frame">
+                        <p class="post_title">タイトル</p>
+                        <p class="post_body">本文</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </body>
+</html>
