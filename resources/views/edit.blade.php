89fcd42b410282110c1bc9c5a738a2f450d0989d
Shota-Amano <ab18004@shibaura-it.ac.jp>
2021-11-17 09:43:19 +0900

    Add pg gem for Heroku

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,44 +1,31 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <!--scripts-->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        
-        <title>Laravel</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+@extends('layouts.app')
+
+@section('stylesheet')
         <link href="{{ asset('css/edit.css') }}" rel="stylesheet">
-        
-    </head>
-    <body>
-        <h1 class="title">編集画面</h1>
-        <div class="content col-md-6 offset-md-3">
-            <form action="/posts/{{ $post->id }}" method="POST">
-                @csrf
-                @method('PUT')
-                    <div class='content__title'>
-                        <h2>タイトル</h2>
-                        <input type='text' name='post[title]' value="{{ $post->title }}">
-                    </div>
-                    <div class='content__body'>
-                        <h2>本文</h2>
-                        <input type='text' name='post[body]' value="{{ $post->body }}">
-                    </div>
-                    @foreach ($tagList as $tag)
-                    <label class="checkbox">
-                        <input type="checkbox" name="tags[]" value="{{$tag->id}}" @if(in_array($tag->id,$tags)) checked @endif>
-                        {{ $tag->name }}
-                    </label>
-                    @endforeach
-                <input type="submit" value="保存">
-            </form>
-        </div>
-        <div class="back col-md-1 offset-md-3">[<a href="/posts">戻る</a>]</div>
-    </body>
-</html>
+@endsection
+
+@section('content')
+    <h1 class="title">編集画面</h1>
+    <div class="content col-md-6 offset-md-3">
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+                <div class='content__title'>
+                    <h2>タイトル</h2>
+                    <input type='text' name='post[title]' value="{{ $post->title }}">
+                </div>
+                <div class='content__body'>
+                    <h2>本文</h2>
+                    <input type='text' name='post[body]' value="{{ $post->body }}">
+                </div>
+                @foreach ($tagList as $tag)
+                <label class="checkbox">
+                    <input type="checkbox" name="tags[]" value="{{$tag->id}}" @if(in_array($tag->id,$tags)) checked @endif>
+                    {{ $tag->name }}
+                </label>
+                @endforeach
+            <input type="submit" value="保存">
+        </form>
+    </div>
+    <div class="back col-md-1 offset-md-3">[<a href="/posts">戻る</a>]</div>
+@endsection
