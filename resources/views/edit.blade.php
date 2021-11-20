f559994aeb307241ad3f23cce7f55bc2700ceac4
Shota-Amano <ab18004@shibaura-it.ac.jp>
2021-11-21 02:49:29 +0900

    20211120

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,27 +5,40 @@
 @endsection
 
 @section('content')
-    <h1 class="title">編集画面</h1>
-    <div class="content col-md-6 offset-md-3">
-        <form action="/posts/{{ $post->id }}" method="POST">
-            @csrf
-            @method('PUT')
-                <div class='content__title'>
-                    <h2>タイトル</h2>
-                    <input type='text' name='post[title]' value="{{ $post->title }}">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="post col-md-6 offset-md-3">
+                <div class="post_title">
+                    <form action="/posts/{{ $post->id }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                            <div class='content__title'>
+                                <h5>タイトル</h5>
+                                <input type='text' name='post[title]' value="{{ $post->title }}">
+                            </div>
+                            <div class='content__body'>
+                                <h5>本文</h5>
+                                <input type='text' name='post[body]' value="{{ $post->body }}">
+                            </div>
+                            @foreach ($tagList as $tag)
+                            <label class="checkbox">
+                                <input type="checkbox" name="tags[]" value="{{$tag->id}}" @if(in_array($tag->id,$tags)) checked @endif>
+                                {{ $tag->name }}
+                            </label>
+                            @endforeach
+                        <input type="submit" value="保存">
+                    </form>
+                    
                 </div>
-                <div class='content__body'>
-                    <h2>本文</h2>
-                    <input type='text' name='post[body]' value="{{ $post->body }}">
-                </div>
-                @foreach ($tagList as $tag)
-                <label class="checkbox">
-                    <input type="checkbox" name="tags[]" value="{{$tag->id}}" @if(in_array($tag->id,$tags)) checked @endif>
-                    {{ $tag->name }}
-                </label>
-                @endforeach
-            <input type="submit" value="保存">
-        </form>
+                
+                
+            </div>
+        </div>
     </div>
-    <div class="back col-md-1 offset-md-3">[<a href="/posts">戻る</a>]</div>
+    <div class="fooder container-fluid">
+        <div class="row">
+        
+            <div class="back col-md-1 offset-md-3">[<a href="/posts">戻る</a>]</div>
+        </div>
+    </div>
 @endsection
