913873ae8e11f91c2f9d2da97a54165e42b12e7f
Shota-Amano <ab18004@shibaura-it.ac.jp>
2021-11-22 19:41:37 +0900

    css追加

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        
         $this->call(UserTableSeeder::class,);
         $this->call(TagsTableSeeder::class,);
         $this->call(PostsTableSeeder::class,);
